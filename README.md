02ce70adba086f7ffc496b6c98e5c512082c2e83
kuche1 <znamsiparolata4esno@abv.bg>
2025-06-19 20:47:58 +0300

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # webshitter
 
-does not even compile
+'''bash
+cargo install trunk
+rustup target add wasm32-unknown-unknown
+trunk serve
+xdg-open http://127.0.0.1:8080/
+'''
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use yew::prelude::*;
+
+struct Model {
+    value: i64,
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    // rerender when the state has changed
+    let state = use_state(|| Model { value: 0 });
+
+    let onclick = {
+        let state = state.clone();
+
+        // functions that need to be called from html need to be
+        // encapsulated in this callback mechanism
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1,
+            })
+        })
+    };
+
+    // `{onclick}` is shorthand for `onclick={onclick}`
+    html! {
+        <div>
+            <button {onclick}>{"+1"}</button>
+            <p>{state.value}</p>
+
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
